b4989769f05f9338f641d599ae5a2f71d8f1cc5b
Carlos Almodóvar Román <Carlos.Almodovar1@alu.uclm.es>
2021-11-24 22:36:36 +0100

    Add files via upload

---
+++ src/main/java/ISO2LAB/A05/App.java
@@ -0,0 +1,13 @@
+package ISO2LAB.A05;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
