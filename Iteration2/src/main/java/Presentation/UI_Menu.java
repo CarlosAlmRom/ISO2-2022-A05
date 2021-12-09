0ff6026ce699594790e0d728c10d9a1d91bb3fa8
Daniel Max Samson Samson <64259707+KillerDMS@users.noreply.github.com>
2021-12-09 12:25:25 +0100

    Reuploading files from Iteration2

---
+++ Iteration2/src/main/java/Presentation/UI_Menu.java
@@ -0,0 +1,20 @@
+package Presentation;
+
+public class UI_Menu {
+
+	public void GetPerson(String Name, String Surname) {
+		// TODO - implement UI_Menu.GetPerson
+		throw new UnsupportedOperationException();
+	}
+
+	public void GetDisease(String DiseaseName) {
+		// TODO - implement UI_Menu.GetDisease
+		throw new UnsupportedOperationException();
+	}
+
+	public void CheckBox(Boolean Mark) {
+		// TODO - implement UI_Menu.CheckBox
+		throw new UnsupportedOperationException();
+	}
+
+}
